6fe2e4d0567b44e5b34de34355b5d734dd82208f
HilmanThoriq <hilmanthoriq@mail.ugm.ac.id>
2025-03-09 14:02:24 +0700

    "First Commit"

---
+++ app/Models/PolylinesModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PolylinesModel extends Model
+{
+    protected $table = 'polyline';
+    
+    protected $guarded = ['id'];
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\PointsController;
+use App\Http\Controllers\TableController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PointsController::class, 'index'])->name('map');
+
+
+Route::get('/table', [TableController::class, 'index'])->name('table');
+
+
+Route::resource('points', PointsController::class);
+
+
+
+
+
+
+// Route::post('/point-store', [PointsController::class, 'store'])->name('point.store');
+
+
+// Route::get('/', function () {
+//     return view('map');
+// });
